144d78d175612e679e3228550c9bec14e1328e6b
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-08-15 18:31:24 +0500

    Implement nextImage() and previousImage()

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
 import "./styles.css";
 import "./modern-css-reset.css";
+
+let offset = 0;
+const numberOfImages = getNumberOfImages();
+let currentlyDisplayedImage = 1;
+createListeners();
+
+function createListeners() {
+  const leftArrow = document.querySelector(".left-arrow");
+  const rightArrow = document.querySelector(".right-arrow");
+  leftArrow.addEventListener("click", () => {
+    previousImage();
+  });
+  rightArrow.addEventListener("click", () => {
+    nextImage();
+  });
+}
+
+function getNumberOfImages() {
+  return document.getElementsByClassName("img").length;
+}
+
+function nextImage() {
+  if (currentlyDisplayedImage + 1 <= numberOfImages) {
+    const width = document.querySelector(".slideshow").offsetWidth;
+    const imageRoll = document.querySelector(".photos");
+    offset += width;
+    currentlyDisplayedImage += 1;
+    imageRoll.style.right = `${offset}px`;
+  }
+}
+
+function previousImage() {
+  if (currentlyDisplayedImage - 1 > 0) {
+    const width = document.querySelector(".slideshow").offsetWidth;
+    const imageRoll = document.querySelector(".photos");
+    offset -= width;
+    currentlyDisplayedImage -= 1;
+    imageRoll.style.right = `${offset}px`;
+  }
+}
